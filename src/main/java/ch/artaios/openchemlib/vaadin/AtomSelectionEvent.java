5ca2f6344d5df4a05c90e4c3d6345f5eb7ff8663
sellnma1 <manuel.sellner@idorsia.com>
2025-02-27 14:36:41 +0100

    feat(OpenChemLibEditor): extended atom selection listener
    Extended the AtomSelectionEvent to support selected atoms in reactions. In REACTION mode, selected atoms are returned as a map with two keys, 'reactants' and 'products'. The values of those are a list of list where the outer list contains molecules and the inner list contains selected atoms.

--- src/main/java/ch/artaios/openchemlib/vaadin/AtomSelectionEvent.java
+++ src/main/java/ch/artaios/openchemlib/vaadin/AtomSelectionEvent.java
@@ -7,27 +7,53 @@ import com.vaadin.flow.component.DomEvent;
 import com.vaadin.flow.component.EventData;
 
 import java.util.List;
+import java.util.Map;
 
 @DomEvent("ocl-selectionchange")
 public class AtomSelectionEvent extends ComponentEvent<OpenChemLibEditor<?>> {
     private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    private final String mode;
     private final String selectedAtomsJson;
 
     public AtomSelectionEvent(
             OpenChemLibEditor<?> source,
             boolean fromClient,
-            @EventData("JSON.stringify(event.detail.selectedAtoms)") String selectedAtomsJson) {
+            @EventData("event.detail.mode") String mode,
+            @EventData("event.detail.selectedAtoms") String selectedAtomsJson) {
         super(source, fromClient);
+        this.mode = mode;
         this.selectedAtomsJson = selectedAtomsJson;
+    }
+
+    public String getMode() {
+        return mode;
     }
 
     public String getSelectedAtomsJson() {
         return selectedAtomsJson;
     }
 
-    public List<Integer> getSelectedAtoms() {
+    public Map<String, List<List<Integer>>> getSelectedReactionAtoms() {
         try {
-            return objectMapper.readValue(selectedAtomsJson, new TypeReference<List<Integer>>() {});
+            if (mode.equals(OpenChemLibEditor.Mode.REACTION.toString().toLowerCase())) {
+                return objectMapper.readValue(selectedAtomsJson, new TypeReference<Map<String, List<List<Integer>>>>() {});
+            } else {
+                return null;
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to parse selected atoms JSON: " + selectedAtomsJson, e);
+        }
+    }
+
+    public List<Integer> getSelectedMoleculeAtoms() {
+        try {
+            if (mode.equals(OpenChemLibEditor.Mode.MOLECULE.toString().toLowerCase())) {
+                return objectMapper.readValue(selectedAtomsJson, new TypeReference<List<Integer>>() {
+                });
+            } else {
+                return null;
+            }
         } catch (Exception e) {
             throw new RuntimeException("Failed to parse selected atoms JSON: " + selectedAtomsJson, e);
         }
