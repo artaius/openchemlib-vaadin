a2697716f5f828f74c233b0cdfecc30ca2237eeb
Roman Bär <roman.baer@idorsia.com>
2025-09-16 17:35:12 +0200

    chore: update copyright

--- src/main/java/ch/artaios/openchemlib/vaadin/ChemUtils.java
+++ src/main/java/ch/artaios/openchemlib/vaadin/ChemUtils.java
@@ -1,3 +1,33 @@
+/*
+ * Copyright (c) 2025 artaius
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 package ch.artaios.openchemlib.vaadin;
 
 import com.actelion.research.chem.IDCodeParser;
@@ -7,23 +37,6 @@ import com.actelion.research.chem.StereoMolecule;
 import com.actelion.research.chem.reaction.Reaction;
 import com.actelion.research.chem.reaction.ReactionEncoder;
 import com.vaadin.flow.function.SerializableBiFunction;
-
-/**
- * Project: openchemlib-vaadin
- * Date:    06.11.24
- * <p>
- * Copyright (c) 2017 - 2024
- * Idorsia Pharmaceuticals Ltd.
- * Hegenheimermattweg 91
- * CH-4123 Allschwil, Switzerland
- * <p>
- * All Rights Reserved.
- * <p>
- * This software is the proprietary information of Idorsia Pharmaceuticals, Ltd.
- * Use is subject to license terms.
- * <p>
- * Author: Roman Bär
- */
 
 /**
  * Utility class for chemical operations.
